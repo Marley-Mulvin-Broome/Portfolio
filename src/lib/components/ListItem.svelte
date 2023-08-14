d4504b7d700900cd0d5c0ae6418e4a5f4e517c18
Marley Mulvin Broome <marley.developer@gmail.com>
2023-08-14 15:25:38 +1000

    Changed video tag for list item to play inline muted with autoplay. Changed the hover behaviour for mobile devices so no style changes

--- src/lib/components/ListItem.svelte
+++ src/lib/components/ListItem.svelte
@@ -10,15 +10,14 @@
     };
 </script>
 
-<li class="text-left rounded-md p-3 hover:translate-x-2 cursor-pointer transition-transform shadow bg-none lg:hover:bg-gradient-to-b lg:hover:from-emerald-600">
+<li class="text-left rounded-md p-3 lg:hover:translate-x-2 cursor-pointer transition-transform shadow bg-none lg:hover:bg-gradient-to-b lg:hover:from-emerald-600">
     <p class="sm:text-m text-3xl underline decoration-4 underline-offset-8 decoration-white-700">{item.name}</p>
     {#if item.image}
         <img class="mt-4 rounded opacity-95" src={item.image} alt="Image of {item.name}" />
     {/if}
     {#if item.video}
-        <video class="mt-4 rounded opacity-95" autoplay loop>
-            <track kind="captions"/>
-            <source src={item.video} type="video/mp4" />
+        <video playsinline muted autoplay loop class="mt-4 rounded opacity-95">
+            <source src={item.video} type="video/webm" />
         </video>
     {/if}
     <p class="mt-2">{item.description}</p>
