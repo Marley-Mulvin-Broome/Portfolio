1181a86b21cab424c46729bfe425fda92ced3782
Marley Mulvin Broome <marley.developer@gmail.com>
2023-08-15 21:03:52 +1000

    Fixed progress tracker not working when transitioning pages, as the size of the page is larger during the transition, with the values being too big.

--- src/lib/components/ScrollProgressTracker.svelte
+++ src/lib/components/ScrollProgressTracker.svelte
@@ -1,20 +1,19 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import { browser } from "$app/environment";
 
     let y = 0;
-    let scrollHeight = 0;
-    let innerHeight = 0;
+    let scrollHeight = 1;
+    let innerHeight = 1;
+    let scrollPercent = 0;
 
-    onMount (() => {
-        scrollHeight = document.body.scrollHeight;
-        innerHeight = window.innerHeight;
-    });
-
-    $: scrollPercent = y / (scrollHeight - innerHeight) * 100;
+    $: if (!browser) {
+        scrollPercent = y / (scrollHeight - innerHeight) * 100;
+    } else {
+        scrollPercent = y / (document.body.scrollHeight - window.innerHeight) * 100;
+    }
 
 </script>
 
 <svelte:window bind:scrollY={y} />
-
 
 <div class="fixed top-0 left-0 h-1 bg-gradient-to-r from-emerald-400 to-emerald-500 z-50" style=" width: {scrollPercent}%;"></div>
